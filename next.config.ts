e52f910595cd9a33f8af3ba78bed168fc853178c
awaisansari06 <muhammadavais14@email.com>
2026-01-11 20:21:11 +0530

    10: Message UI

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+// next-config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false, // This will hide the indicator logo
 };
 
 export default nextConfig;
