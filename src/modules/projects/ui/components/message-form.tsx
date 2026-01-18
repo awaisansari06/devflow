b70c39ada42186270ba9649d6199bad50d1c40a4
awaisansari06 <muhammadavais14@email.com>
2026-01-18 14:23:51 +0530

    17- Credit Usage

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -6,10 +6,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import TextareaAutosize from "react-textarea-autosize";
 import { ArrowUpIcon, Loader2Icon } from "lucide-react";
 import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Form, FormField } from "@/components/ui/form";
+import { Usage } from "@/modules/projects/ui/components/usage";
 
 
 
@@ -25,7 +27,11 @@ const formSchema = z.object({
 
 export const MessageForm = ({ projectId }: Props) => {
     const trpc = useTRPC();
+    const router = useRouter();
     const queryClient = useQueryClient();
+
+    const { data: usage } = useQuery(trpc.usage.status.queryOptions());
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -39,11 +45,16 @@ export const MessageForm = ({ projectId }: Props) => {
             queryClient.invalidateQueries(
                 trpc.messages.getMany.queryOptions({projectId})
             );
-            // TODO: Invalidate usage status
+            queryClient.invalidateQueries(
+                trpc.usage.status.queryOptions()
+            );
         },
         onError: (error) => {
-            // TODO: Redirect to pricing page if specific error
             toast.error(error.message);
+
+            if(error.data?.code === "TOO_MANY_REQUESTS") {
+                router.push("/pricing");
+            }
         },
     }))
     
@@ -56,11 +67,17 @@ export const MessageForm = ({ projectId }: Props) => {
     
     const [isFocused, setIsFocused] = useState(false);
     const isPending = createMessage.isPending;
-    const showUsage = false;
     const isButtonDisabled = isPending || !form.formState.isValid;
+    const showUsage = !!usage;
 
     return (
         <Form {...form}>
+            {showUsage && (
+                <Usage
+                    points={usage.remainingPoints}
+                    msBeforeNext={usage.msBeforeNext}
+                />
+            )}
             <form
                 onSubmit={form.handleSubmit(onSubmit)}
                 className={cn(
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { usageRouter } from '@/modules/usage/server/procedures';
 import { createTRPCRouter } from '../init';
 import { messageRouter } from '@/modules/messages/server/procedures';
 import { projectsRouter } from '@/modules/projects/server/procedures';
 
 export const appRouter = createTRPCRouter({
+  usage: usageRouter,
   messages: messageRouter,
   projects: projectsRouter,
 });
